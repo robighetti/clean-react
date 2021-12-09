473e872b9575ba92f05eb8491f7df1204a700593
Rodrigo Bighetti <robighetti@gmail.com>
2021-12-09 16:13:48 -0300

    refactor: mocks-http-clients added

--- src/data/useCases/authentication/remote-authentication.spec.ts
+++ src/data/useCases/authentication/remote-authentication.spec.ts
@@ -1,17 +1,8 @@
-import { HttpPostClient } from 'data/protocols/http/http-post-client'
+import { HttpPostClientSpy } from '../../tests/mocks-http-clients'
 import { RemoteAuthentication } from './remote-authentication'
 
 describe('RemoteAuthentication', () => {
   test('should call HttpPostClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-
-      async post (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
-
     const url = 'any_url'
     const httpPostClientSpy = new HttpPostClientSpy()
 
