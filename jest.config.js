84782c7b4473c1f7d395c4825d14388f7ffee7bd
Rodrigo Bighetti <robighetti@gmail.com>
2021-12-09 14:49:26 -0300

    chore: config jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable semi */
+/* eslint-disable comma-dangle */
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
